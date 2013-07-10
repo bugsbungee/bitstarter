e80e05902dade730d8b154a7ddfd3deff784e698
Ubuntu <ubuntu@ip-10-48-110-125.eu-west-1.compute.internal>
2013-07-10 08:25:31 +0000

    Parse url from command line

--- grader.js
+++ grader.js
@@ -10,8 +10,10 @@ development and basic DOM parsing.
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://localhost";
 var assertFileExists = function(infile) {
 	var intr = infile.toString();
 	if(!fs.existsSync(instr)) {
@@ -21,6 +23,10 @@ var assertFileExists = function(infile) {
 	return instr;
 };
 
+var assertUrlExists = function(inurl){
+	console.log("Checking Url");
+	return inurl.toString();
+};
 var cheerioHtmlFile = function(htmlfile) {
 	return cheerio.load(fs.readFileSync(htmlfile));
 };
@@ -40,6 +46,15 @@ var checkHtmlFile = function(htmlfile, checksfile) {
 	return out;
 };
 
+var checkHtmlUrl = function(htmlurl, checksfile) {
+	var htmlresult = '';
+	console.log('Url ' + htmlurl.toString());
+	rest.get(htmlurl).on('complete', function(result){
+		htmlresult = result;
+	});
+	return htmlresult;	
+};
+
 var clone = function(fn) {
 	return fn.bind({});
 };
@@ -48,10 +63,20 @@ if(require.main == module) {
 	program
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url>', 'Url to web page', clone(assertUrlExists))
 	.parse(process.argv);
-	var checkJson = checkHtmlFile(program.file, program.checks);
-	var outJson = JSON.stringify(checkJson, null, 4);
-	console.log(outJson);
+	
+	if(!program.url) {
+		console.log('Url is null');
+		var checkJson = checkHtmlFile(program.file, program.checks);
+		var outJson = JSON.stringify(checkJson, null, 4);
+		console.log(outJson);
+	} else {
+		// Check the Url
+		console.log("URL = "+ program.url.toString());
+		console.log(checkHtmlUrl(program.url.toString()));
+	}
+	
 
 } else {
 	exports.checkHtmlFile = checkHtmlFile;
